33b15e6626e8528e4c99d5edeb6d49e9269e4be2
nabin <nabingautam1021@gmail.com>
2024-06-04 09:27:12 +0530

    4SF23CI405

---
+++ nabin.py
@@ -0,0 +1,12 @@
+
+num1 = input("12: ")
+num2 = input("14: ")
+
+
+num1 = float(21)
+num2 = float(32)
+
+sum = num1 + num2
+
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+print("4SF23CI405")
